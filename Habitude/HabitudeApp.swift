1dcd4e32ff372eb5dc6dbdd59c5bfaef3eff29ff
Nathan Dufils <dufilsnathan@gmail.com>
2024-03-06 09:43:45 +0100

    Initial Commit

---
+++ Habitude/HabitudeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitudeApp.swift
+//  Habitude
+//
+//  Created by Dufils Nathan on 06/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct HabitudeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
