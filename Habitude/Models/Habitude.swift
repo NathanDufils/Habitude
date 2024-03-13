7d8f5d978ada88a532eb1bf9dd9712d91b641c85
blorton <baptiste.lorton@gmail.com>
2024-03-13 09:04:07 +0100

    test

--- Habitude/Models/Habitude.swift
+++ Habitude/Models/Habitude.swift
@@ -1,8 +1,22 @@
-//
-//  Habitude.swift
-//  Habitude
-//
-//  Created by Dufils Nathan on 13/03/2024.
-//
-
 import Foundation
+
+enum Type: String, CaseIterable {
+    case g = "g"
+    case ml = "ml"
+    case km = "km"
+    case n = "none"
+}
+
+struct Habitude: Identifiable {
+    var id = UUID()
+    var title: String
+    var quantity: Double
+    var type: Type
+    var state: Bool
+    
+    static var testData = [
+        Habitude(title: "Boire de l'eau", quantity: 1000, type: .ml, state: false),
+        Habitude(title: "Faire des pompes", quantity: 10,type: .n, state: true),
+        Habitude(title: "Matchs Tinder", quantity: 5, type: .n, state: true),
+    ]
+}
