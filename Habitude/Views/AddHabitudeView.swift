da2c8f3537afa33013c0d4d12297ab7ef8d9468d
Tom Bertomeu <tom.bertomeu@edu.univ-fcomte.fr>
2024-03-20 08:31:57 +0100

    Création AddHabitudeView

--- Habitude/Views/AddHabitudeView.swift
+++ Habitude/Views/AddHabitudeView.swift
@@ -5,4 +5,45 @@
 //  Created by bertomeu tom on 20/03/2024.
 //
 
-import Foundation
+import SwiftUI
+
+struct AddHabitudeView: View {
+    
+    @State var habitudeTitle: String = ""
+    
+    @EnvironmentObject var data: HabitudeViewModel
+    
+    var body: some View {
+        VStack(spacing: 16) {
+            TextField("Entrer votre habitude", text: $habitudeTitle)
+                .padding(.horizontal)
+                .frame(height: 55)
+                .background(Color(.systemGray5))
+                .cornerRadius(10)
+            
+            Button {
+                
+            } label: {
+                Text("Ajouter")
+                    .foregroundStyle(.white)
+                    .font(.headline)
+                    .frame(height: 55)
+                    .frame(maxWidth: .infinity)
+                    .background(.blue)
+                    .cornerRadius(10)
+            }
+            
+            Spacer()
+        }
+        .padding(14)
+        .navigationTitle("Ajouter une habitude")
+    }
+}
+
+struct AddHabitudeView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            AddHabitudeView()
+        }
+    }
+}
