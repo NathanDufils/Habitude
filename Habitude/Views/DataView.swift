8e1a4008e5bd696b5baab98978a144437a23eee7
blorton <baptiste.lorton@gmail.com>
2024-03-27 09:54:08 +0100

    debut visu

--- Habitude/Views/DataView.swift
+++ Habitude/Views/DataView.swift
@@ -8,25 +8,45 @@
 import SwiftUI
 
 struct DataView: View {
-    @State var selectedDate: Date = Date()
-    
+    struct DepartmentProfit {
+        var department: String
+        var profit: Double
+    }
+
+    var data: [DepartmentProfit] = [
+        DepartmentProfit(department: "LUN", profit: 3),
+        DepartmentProfit(department: "MAR", profit: 4),
+        DepartmentProfit(department: "MER", profit: 5),
+        DepartmentProfit(department: "JEU", profit: 2),
+        DepartmentProfit(department: "VEN", profit: 1),
+        DepartmentProfit(department: "SAM", profit: 6),
+        DepartmentProfit(department: "DIM", profit: 3)
+    ]
+
     var body: some View {
-        VStack() {
-            Text(selectedDate.formatted(date: .abbreviated, time: .omitted))
-                .font(.system(size: 28))
-                .bold()
-                .foregroundColor(Color.accentColor)
-                .padding()
-                .animation(.spring(), value: selectedDate)
-                .frame(width: 500)
-            Divider().frame(height: 1)
-            DatePicker("Select Date", selection: $selectedDate, displayedComponents: [.date])
-                .padding(.horizontal)
-                .datePickerStyle(.graphical)
-            Divider()
-            
+        HStack {
+            ForEach(data, id: \.department) { department in
+                BarView(value: department.profit, label: department.department)
+            }
         }
-        .padding(.vertical, 100)
+        .padding()
+    }
+}
+
+struct BarView: View {
+    var value: Double
+    var label: String
+
+    var body: some View {
+        VStack {
+            Text(String(format: "%.0f", value))
+            Rectangle()
+                .fill(value > 2 ? Color.green : Color.red)
+                .frame(width: 30, height: CGFloat(value*25))
+                .padding(10)
+            Text(label)
+        }
+        .padding(.bottom, 10)
     }
 }
 
